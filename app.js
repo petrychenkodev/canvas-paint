40353e5c80fd8526e31338ecf2e9408a8e695c1b
Petrychenkodev <panleshapan@gmail.com>
2021-05-11 19:07:32 +0300

    rename

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const strokeThickness = document.querySelector("#size");
 const colorBtn = document.querySelector("#color");
 const clearBtn = document.querySelector("#clear");
 
-/*
-******************* Canvas getContext() Method *********************
-https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext#:~:text=getContext()%20method%20returns%20a,to%20a%20different%20context%20mode.
-
-******************* CanvasRenderingContext2D ********************
-https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
-*/
-
 const ctx = canvas.getContext("2d");
 
 let size = 10;
